af481d528c117d84aeb1e79e48398f555aa5d848
鮫島泰史 <h.sameshima@wiz-net.jp>
2024-12-22 15:14:26 +0900

    first commit

--- lib/my-cdk-s3-stack.ts
+++ lib/my-cdk-s3-stack.ts
@@ -1,6 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+// dotenv
+import * as dotenv from "dotenv";
+
+// dotenvのconfigを呼び出す
+dotenv.config();
 
 export class MyCdkS3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +18,18 @@ export class MyCdkS3Stack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'MyCdkS3Queue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // 環境変数からバケット名を取得
+    const bucketName = process.env.BUCKET_NAME;
+
+    if (!bucketName) {
+      throw new Error("BUCKET_NAMEが環境変数に定義されていません");
+    }
+
+    // create a bucket
+    const bucket = new s3.Bucket(this, bucketName, {
+      versioned: true, // バージョニングを有効化
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // スタック削除時にバケットを削除
+    });
   }
 }
